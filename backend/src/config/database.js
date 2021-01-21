1fcf55b201ccda8aef763451c5c10b2025ee2e63
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-21 11:09:04 -0300

    :bug: Mongoose Bug
    Fixed Bugs about mongoose upper versions

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -5,4 +5,6 @@ mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório!
 mongoose.Error.messages.String.enum =
   "'{VALUE}' não é válido para o atributo '{PATH}' ";
 
-module.exports = mongoose.connect('mongodb://localhost:27017/mymoney');
+module.exports = mongoose.connect('mongodb://localhost:27017/mymoney', {
+  useNewUrlParser: true,
+});
