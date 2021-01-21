cc5d6bc7ca9c03c0b4933470ec2a9a00f71ccc3a
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-21 11:02:23 -0300

    :art: Validators
    Improved validation erros

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório!";
+mongoose.Error.messages.String.enum =
+  "'{VALUE}' não é válido para o atributo '{PATH}' ";
+
 module.exports = mongoose.connect('mongodb://localhost:27017/mymoney');
