30e6a94721632241892aacc42fa9b73dec257c9f
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-22 10:40:03 -0300

    :sparkles: QueryParser
    Added QueryParser.

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,11 +2,14 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('./cors');
+const queryParser = require('express-query-int');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors);
+app.use(queryParser());
 
 app.listen(port, () => {
   console.log(`BACKEND is running on ${port}`);
