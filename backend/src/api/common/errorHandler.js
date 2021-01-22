fb53955e82897b3eb34a8f439fb7fe7ab89e71e3
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-22 10:30:06 -0300

    :art: CORS and bugs
    Added CORS headers and fixed some bugs with errorHandler.

--- backend/src/api/common/errorHandler.js
+++ backend/src/api/common/errorHandler.js
@@ -1,7 +1,8 @@
-const _ = require('loadash');
+const _ = require('lodash');
 
 module.exports = (req, res, next) => {
   const bundle = res.locals.bundle;
+
   if (bundle.errors) {
     const errors = parseErrors(bundle.errors);
     res.status(500).json({ errors });
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,12 @@
 const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('./cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors);
 
 app.listen(port, () => {
   console.log(`BACKEND is running on ${port}`);
