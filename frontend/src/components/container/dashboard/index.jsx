4e430ddecc2ea16f33b5b6d4bf95af26ef613f26
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-26 11:39:07 -0300

    :art: Dashboard
    Done Dashboard Component

--- frontend/src/components/container/dashboard/index.jsx
+++ frontend/src/components/container/dashboard/index.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 
 import Content from '../../../common/template/content/index'
 import ContentHeader from '../../../common/template/content/contentHeader'
+import ValueBox from '../../../common/template/widget/valueBox'
+import Row from '../../../common/template/layout/row'
 
  class DashBoard extends Component {
 
@@ -10,7 +12,11 @@ import ContentHeader from '../../../common/template/content/contentHeader'
       <div>
         <ContentHeader title="Dashboard" subTitle="versão 1.0"/>
         <Content>
-          DashBoard
+          <Row>
+          <ValueBox cols="12 4" bgColor="green" icon="bank" value="R$ 10" text="Total de Créditos"/>
+          <ValueBox cols="12 4" bgColor="red" icon="credit-card" value="R$ 10" text="Total de Débitos"/>
+          <ValueBox cols="12 4" bgColor="blue" icon="money" value="R$ 0" text="Valor Consolidado"/>
+          </Row>
         </Content>
       </div>
     )
