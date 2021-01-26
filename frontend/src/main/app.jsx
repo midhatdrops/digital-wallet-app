99e0a54833a8a97bdea9441d0d28a28719c996e5
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-26 10:13:19 -0300

    :sparkles: Template
    Created Header and imported into App

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../common/template/dependecies.js'
+import Header from '../common/template/header/index'
 
 export default props => {
   return(<div className="wrapper">
-    <h1>App</h1>
+    <Header/>
   </div>)
 }
