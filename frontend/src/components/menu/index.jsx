ed109b9ec72a884ffadeb26fa8a9d16264ec2c88
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-26 10:30:13 -0300

    :sparkles: Template
    Created Menu component and imported into App.

---
+++ frontend/src/components/menu/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
+
+
+export default props => (
+  <ul className="sidebar-menu">
+    <MenuItem path="#" icon="dashboard" label="Dashboard"/>
+    <MenuTree label="Cadastro" icon="edit" >
+      <MenuItem path="#billingCycles" label="Ciclos de Pagamento" icon="usd"/>
+    </MenuTree>
+  </ul>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../common/template/dependecies.js'
 import Header from '../common/template/header/index'
+import Sidebar from '../common/template/sidebar/index'
 
 export default props => {
   return(<div className="wrapper">
     <Header/>
+    <Sidebar />
   </div>)
 }
