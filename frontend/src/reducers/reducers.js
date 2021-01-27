157cb6e3024b8969b8284113bb6e19ee26f7b0ad
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-27 10:52:28 -0300

    :sparkles: Billing Cycle Redux
    Created Action and Reducer for Billing Cycle, todo Billing Cycle List.

--- frontend/src/reducers/reducers.js
+++ frontend/src/reducers/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import DashboardReducer from './dashboardReducer';
 import TabReducer from './tabReducer';
+import BillingCyclesReducer from './billingCyclesReducer';
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
   tab: TabReducer,
+  BillingCycle: BillingCyclesReducer,
 });
 
 export default rootReducer;
