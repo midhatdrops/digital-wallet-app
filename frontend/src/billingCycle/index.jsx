0f5da090d57b3bdac4330a92c6be29f9462acaa1
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-27 09:47:05 -0300

    :sparkles: Tab Redux
    Created Reducer and ActionCreator for Tab Component

--- frontend/src/billingCycle/index.jsx
+++ frontend/src/billingCycle/index.jsx
@@ -6,6 +6,8 @@ import Tabs from '../common/template/tabs/index'
 import TabsContent from '../common/template/tabs/tabsContent'
 import TabsHeader from '../common/template/tabs/tabsHeader'
 
+import TabHeader from '../common/template/tabs/Header'
+
 export default class BillingCycle extends Component {
 
   render() {
@@ -14,7 +16,10 @@ export default class BillingCycle extends Component {
       <Content>
         <Tabs>
           <TabsHeader>
-            
+            <TabHeader icon="bars" label="Listar" target="tabList"/>
+            <TabHeader icon="plus" label="Incluir" target="tabCreate"/>
+            <TabHeader icon="pencil" label="Alterar" target="tabUpdate"/>
+            <TabHeader icon="trash-o" label="Excluir" target="tabDelete"/>
           </TabsHeader>
           <TabsContent>
               
--- frontend/src/reducers/reducers.js
+++ frontend/src/reducers/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import DashboardReducer from './dashboardReducer';
+import TabReducer from './tabReducer';
 
 const rootReducer = combineReducers({
   dashboard: DashboardReducer,
+  tab: TabReducer,
 });
 
 export default rootReducer;
