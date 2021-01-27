67abc6d572f29c1a362eee2e38401791b2f0d189
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-27 09:06:27 -0300

    :sparkles: Redux middlewares
    Integrated Axios async calls to promise middleware

--- frontend/src/components/container/dashboard/index.jsx
+++ frontend/src/components/container/dashboard/index.jsx
@@ -1,4 +1,8 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
+import {getSummary} from '../../../reducers/actions/dashboardActions'
 
 import Content from '../../../common/template/content/index'
 import ContentHeader from '../../../common/template/content/contentHeader'
@@ -7,15 +11,20 @@ import Row from '../../../common/template/layout/row'
 
  class DashBoard extends Component {
 
+  componentWillMount() {
+    this.props.getSummary()
+  }
+
   render() {
+    const {credit,debt} = this.props.summary
     return(
       <div>
         <ContentHeader title="Dashboard" subTitle="versão 1.0"/>
         <Content>
           <Row>
-          <ValueBox cols="12 4" bgColor="green" icon="bank" value="R$ 10" text="Total de Créditos"/>
-          <ValueBox cols="12 4" bgColor="red" icon="credit-card" value="R$ 10" text="Total de Débitos"/>
-          <ValueBox cols="12 4" bgColor="blue" icon="money" value="R$ 0" text="Valor Consolidado"/>
+          <ValueBox cols="12 4" bgColor="green" icon="bank" value={`R$ ${credit}`} text="Total de Créditos"/>
+          <ValueBox cols="12 4" bgColor="red" icon="credit-card" value={`R$ ${debt}`} text="Total de Débitos"/>
+          <ValueBox cols="12 4" bgColor="blue" icon="money" value={`R$ ${credit - debt}`} text="Valor Consolidado"/>
           </Row>
         </Content>
       </div>
@@ -23,4 +32,8 @@ import Row from '../../../common/template/layout/row'
   }
 }
 
-export default DashBoard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashBoard)
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,7 @@
 import { createStore } from 'redux';
 import Reducers from '../reducers/reducers';
 
-export const store = createStore(Reducers);
+import { applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
+export const store = applyMiddleware(promise)(createStore)(Reducers);
