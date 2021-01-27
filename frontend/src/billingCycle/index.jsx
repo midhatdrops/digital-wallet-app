32eff8cb650a795332b3d115ee80c0659de4bbae
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-27 09:32:38 -0300

    :sparkles: Tabs
    Created Tabs Grid, Tabs Header and content, and instancied into BillingCycle component

--- frontend/src/billingCycle/index.jsx
+++ frontend/src/billingCycle/index.jsx
@@ -2,13 +2,24 @@ import React, {Component} from 'react'
 import ContentHeader from '../common/template/content/contentHeader'
 import Content from '../common/template/content/index'
 
+import Tabs from '../common/template/tabs/index'
+import TabsContent from '../common/template/tabs/tabsContent'
+import TabsHeader from '../common/template/tabs/tabsHeader'
+
 export default class BillingCycle extends Component {
 
   render() {
     return(<div>
       <ContentHeader title="Ciclos de Pagamento" subTitle="Cadastro"/>
       <Content>
-        Ciclos de Pagamento
+        <Tabs>
+          <TabsHeader>
+            
+          </TabsHeader>
+          <TabsContent>
+              
+            </TabsContent>
+        </Tabs>
       </Content>
     </div>)
   }
