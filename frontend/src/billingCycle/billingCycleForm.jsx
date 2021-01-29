c31ace45e1569ba4155f639348ca574f7c3baae4
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-29 12:26:46 -0300

    :art: Status Field
    Added StatusFieldd to ItemList Component

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -4,19 +4,20 @@ import {bindActionCreators} from 'redux'
 import {init} from '../reducers/actions/billingCyclesActions'
 import {reduxForm, Field, formValueSelector} from 'redux-form'
 import InputLabel from '../common/template/form/inputAndLabel'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
 
   render() {
-    const {handleSubmit, readOnly, credits} = this.props
+    const {handleSubmit, readOnly, credits, debts} = this.props
     return(
       <form role="form" onSubmit={handleSubmit}>
           <div className="box-body">
             <Field name="name" component={InputLabel} cols="12 4" label="Nome" placeholder="Informe o nome" readOnly={readOnly}/>
             <Field name="month" component={InputLabel} cols="12 4" type="number" placeholder="Informe o mês" label="Mês" readOnly={readOnly}/>
             <Field name="year" component={InputLabel} cols="12 4" type="number" placeholder="Informe o ano" label="Ano" readOnly={readOnly}/>
-            <CreditList cols="12 6" list={credits} readOnly={readOnly}/>
+            <ItemList cols="12 6" list={credits} readOnly={readOnly} field="credits" legend="Créditos"/>
+            <ItemList cols="12 6" list={debts} readOnly={readOnly} field="debts" legend="Débitos" showStatus={true}/>
           </div>
           <div className="box-footer">
             <button type="submit" className={`btn btn-${this.props.submitClass}`}>{`${this.props.submitLabel}`}</button>
@@ -33,7 +34,8 @@ BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false}
 
 const selector = formValueSelector('billingCycleForm')
 const mapStateToProps = state => ({
-  credits: selector(state,'credits')
+  credits: selector(state,'credits'),
+  debts: selector(state,'debts')
 })
 
 export default connect(mapStateToProps,mapDispatchToProps)(BillingCycleForm)
