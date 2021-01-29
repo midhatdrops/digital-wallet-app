6a4c45bbe4ec31056a8287fac9230585f985012a
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-29 12:42:20 -0300

    :art: CORS Policy
    Updated CORS Policy

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,11 +1,11 @@
 module.exports = (req, res, next) => {
-  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    'Acess-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, DELETE, PATCH'
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
   res.header(
-    'Acess-Control-Allow-Headers',
+    'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
