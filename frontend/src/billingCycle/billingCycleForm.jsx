ef5f2dd7840a2bd7762929e3d26f0af954e5c43f
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-29 12:41:43 -0300

    :sparkles: Summary Component
    Introduced Summary Component into BillingCycle

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,17 +5,28 @@ import {init} from '../reducers/actions/billingCyclesActions'
 import {reduxForm, Field, formValueSelector} from 'redux-form'
 import InputLabel from '../common/template/form/inputAndLabel'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
+  calculateSummary() {
+    const sum = (total,actualValue) => total + actualValue
+    return {
+      sumOfCredits: this.props.credits.map(c => (+c.value || 0)).reduce(sum),
+      sumOfDebts: this.props.debts.map(d => (+d.value || 0)).reduce(sum)
+    }
+  }
+
   render() {
     const {handleSubmit, readOnly, credits, debts} = this.props
+    const {sumOfCredits, sumOfDebts} = this.calculateSummary()
     return(
       <form role="form" onSubmit={handleSubmit}>
           <div className="box-body">
             <Field name="name" component={InputLabel} cols="12 4" label="Nome" placeholder="Informe o nome" readOnly={readOnly}/>
             <Field name="month" component={InputLabel} cols="12 4" type="number" placeholder="Informe o mês" label="Mês" readOnly={readOnly}/>
             <Field name="year" component={InputLabel} cols="12 4" type="number" placeholder="Informe o ano" label="Ano" readOnly={readOnly}/>
+            <Summary credit={sumOfCredits} debt={sumOfDebts}/>
             <ItemList cols="12 6" list={credits} readOnly={readOnly} field="credits" legend="Créditos"/>
             <ItemList cols="12 6" list={debts} readOnly={readOnly} field="debts" legend="Débitos" showStatus={true}/>
           </div>
