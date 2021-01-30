b2004fd52b026706a00a5a6dc9182912fb0ee1eb
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-30 12:35:58 -0300

    :sparkles: Authenticate Component
    Created Authenticate component and integrated with Routes and App

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import '../common/template/dependecies.js'
+import React from 'react';
 
-import Header from '../common/template/header/index'
-import Sidebar from '../common/template/sidebar/index'
-import Footer from '../common/template/footer/index'
+import Header from '../common/template/header/index';
+import Sidebar from '../common/template/sidebar/index';
+import Footer from '../common/template/footer/index';
 
+import Messages from '../common/msg/messages';
 
-import Messages from '../common/msg/messages'
-
-export default props => {
-  return(<div className="wrapper">
-    <Header/>
-    <Sidebar />
-    <div className="content-wrapper">
-      {props.children}
+export default (props) => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Sidebar />
+      <div className="content-wrapper">{props.children}</div>
+      <Footer />
+      <Messages />
     </div>
-    <Footer/>
-    <Messages />
-  </div>)
-}
+  );
+};
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import {Router, Route, Redirect, IndexRoute,  hashHistory} from 'react-router'
+import React from 'react';
+import { Router, Route, Redirect, IndexRoute, hashHistory } from 'react-router';
+import AuthOrApp from './authOrApp';
 
-import App from './app'
+import App from './app';
 
-import DashBoard from '../components/container/dashboard/index'
-import BillingCycle from '../billingCycle/index'
+import DashBoard from '../components/container/dashboard/index';
+import BillingCycle from '../billingCycle/index';
 
-export default props => (
+export default (props) => (
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={DashBoard} />
-      <Route path="billingCycles" component={BillingCycle}/> 
+      <Route path="billingCycles" component={BillingCycle} />
+      <Route path="/" component={AuthOrApp}></Route>
     </Route>
-    <Redirect from='*' to="/" />
+    <Redirect from="*" to="/" />
   </Router>
-)
+);
